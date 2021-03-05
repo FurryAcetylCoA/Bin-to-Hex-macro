b54efee43e24031eb57189c684eeac6b86dc258a
FurryAcetylCoA <68182718+FurryAcetylCoA@users.noreply.github.com>
2021-03-05 18:04:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ f = open('BintoHex.h', mode='w')
 f.write("//A C header converting 8-bit binary literal into hexadecimal literal. For some old C compiler \n")
 f.write("#ifndef __BintoHex_h__\n#define __BintoHex_h__\n\n")
 for HEX in range(0, 256):
-    f.write(f"#define {HEX:#04x} {HEX:#010b}")
+    f.write(f"#define {HEX:#010b} {HEX:#04x} ")
     f.write('\n')
 f.write("\n#endif //__BintoHex_h__")
 f.close()
